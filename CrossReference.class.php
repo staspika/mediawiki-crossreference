ddced230d7dfabbbba09113506d360e8e2f079c5
paladox <paladox@users.noreply.github.com>
2020-10-15 23:20:35 +0100

    Fix "PHP Warning:  Parameter 2 to ExtCrossReference::parserAfterTidy() expected to be a reference, value given in /srv/mediawiki/w/extensions/CrossReference/CrossReference.php on line 99"
    Fixes compatibility with mediawiki 1.35

--- CrossReference.class.php
+++ CrossReference.class.php
@@ -189,7 +189,7 @@ class ExtCrossReference
 
     /** Invoked at the last stage of the p  arsing process.
       */
-    public function parserAfterTidy( &$parser, &$text )
+    public function parserAfterTidy( $parser, &$text )
     {
         $text = preg_replace("/&(amp;)?column;/s", ':', $text);
         // Put back captions
